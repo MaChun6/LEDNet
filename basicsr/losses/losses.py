4bb3eecc816845a444198bdc9dc171e95e4c4ace
Haigen Liu <129713016+MaChun6@users.noreply.github.com>
2024-08-19 18:29:56 +0800

    Update losses.py

--- basicsr/losses/losses.py
+++ basicsr/losses/losses.py
@@ -4,11 +4,15 @@ import torch
 from torch import autograd as autograd
 from torch import nn as nn
 from torch.nn import functional as F
-
+from torch.autograd import Variable
+from math import exp
+import numpy as np
+from torchvision import models
 from basicsr.archs.vgg_arch import VGGFeatureExtractor
 from basicsr.utils.registry import LOSS_REGISTRY
 from .loss_util import weighted_loss
-
+import torchvision.models as models
+import numpy as np
 _reduction_modes = ['none', 'mean', 'sum']
 
 
@@ -255,8 +259,8 @@ class PerceptualLoss(nn.Module):
 
 @LOSS_REGISTRY.register()
 class LPIPSLoss(nn.Module):
-    def __init__(self, 
-            loss_weight=1.0, 
+    def __init__(self,
+            loss_weight=1.0,
             use_input_norm=True,
             range_norm=False,):
         super(LPIPSLoss, self).__init__()
@@ -453,3 +457,274 @@ def gradient_penalty_loss(discriminator, real_data, fake_data, weight=None):
         gradients_penalty /= torch.mean(weight)
 
     return gradients_penalty
+
+@LOSS_REGISTRY.register()
+class FFTLoss(nn.Module):
+
+    def __init__(self, loss_weight=1.0, reduction='mean', eps=1e-12):
+        super(FFTLoss, self).__init__()
+        if reduction not in ['none', 'mean', 'sum']:
+            raise ValueError(f'Unsupported reduction mode: {reduction}. ' f'Supported ones are: {_reduction_modes}')
+
+        self.loss_weight = loss_weight
+        self.reduction = reduction
+        self.eps = eps
+
+    def forward(self, pred, target, weight=None, **kwargs):
+        pred_fft = torch.fft.rfft2(pred)
+        label_fft = torch.fft.rfft2(target)
+        loss = F.l1_loss(pred_fft, label_fft, reduction='none').mean()
+        return loss * self.loss_weight, None
+
+@LOSS_REGISTRY.register()
+class FFTWeightLoss(nn.Module):
+
+    def __init__(self, loss_weight=1.0, reduction='mean', eps=1e-12):
+        super(FFTWeightLoss, self).__init__()
+        if reduction not in ['none', 'mean', 'sum']:
+            raise ValueError(f'Unsupported reduction mode: {reduction}. ' f'Supported ones are: {_reduction_modes}')
+
+        self.loss_weight = loss_weight
+        self.reduction = reduction
+        self.eps = eps
+
+    def forward(self, pred, target, weight=None, **kwargs):
+        pred = pred * weight
+        target = target * weight
+        pred_fft = torch.fft.rfft2(pred)
+        label_fft = torch.fft.rfft2(target)
+        loss = F.l1_loss(pred_fft, label_fft, reduction='none').mean()
+        return loss * self.loss_weight
+
+class EdgeLoss(nn.Module):
+    def __init__(self):
+        super(EdgeLoss, self).__init__()
+        k = torch.Tensor([[.05, .25, .4, .25, .05]])
+        self.kernel = torch.matmul(k.t(), k).unsqueeze(0).repeat(3, 1, 1, 1)
+        if torch.cuda.is_available():
+            self.kernel = self.kernel.cuda()
+        self.loss = CharbonnierLoss()
+
+    def conv_gauss(self, img):
+        n_channels, _, kw, kh = self.kernel.shape
+        img = F.pad(img, (kw // 2, kh // 2, kw // 2, kh // 2), mode='replicate')
+        return F.conv2d(img, self.kernel, groups=n_channels)
+
+    def laplacian_kernel(self, current):
+        filtered = self.conv_gauss(current)  # filter
+        down = filtered[:, :, ::2, ::2]  # downsample
+        new_filter = torch.zeros_like(filtered)
+        new_filter[:, :, ::2, ::2] = down * 4  # upsample
+        filtered = self.conv_gauss(new_filter)  # filter
+        diff = current - filtered
+        return diff
+
+    def forward(self, x, y):
+        # x = torch.clamp(x + 0.5, min = 0,max = 1)
+        # y = torch.clamp(y + 0.5, min = 0,max = 1)
+        loss = self.loss(self.laplacian_kernel(x), self.laplacian_kernel(y))
+        return loss
+
+
+class ContrastLoss(nn.Module):
+    def __init__(self, ablation=False):
+
+        super(ContrastLoss, self).__init__()
+        self.vgg = Vgg19().cuda()
+        self.l1 = nn.L1Loss()
+        self.ab = ablation
+        self.down_sample_4 = nn.Upsample(scale_factor=1 / 4, mode='bilinear')
+    def forward(self, restore, sharp, blur):
+        B, C, H, W = restore.size()
+        restore_vgg, sharp_vgg, blur_vgg = self.vgg(restore), self.vgg(sharp), self.vgg(blur)
+
+        # filter out sharp regions
+        threshold = 0.01
+        mask = torch.mean(torch.abs(sharp-blur), dim=1).view(B, 1, H, W)
+        mask[mask <= threshold] = 0
+        mask[mask > threshold] = 1
+        mask = self.down_sample_4(mask)
+        d_ap = torch.mean(torch.abs((restore_vgg - sharp_vgg.detach())), dim=1).view(B, 1, H//4, W//4)
+        d_an = torch.mean(torch.abs((restore_vgg - blur_vgg.detach())), dim=1).view(B, 1, H//4, W//4)
+        mask_size = torch.sum(mask)
+        contrastive = torch.sum((d_ap / (d_an + 1e-7)) * mask) / mask_size
+
+        return contrastive
+
+class Vgg19(torch.nn.Module):
+    def __init__(self, requires_grad=False):
+        super(Vgg19, self).__init__()
+        vgg_pretrained_features = models.vgg19(pretrained=True).features
+        self.slice1 = torch.nn.Sequential()
+
+        for x in range(12):
+            self.slice1.add_module(str(x), vgg_pretrained_features[x])
+
+        if not requires_grad:
+            for param in self.parameters():
+                param.requires_grad = False
+
+    def forward(self, X):
+        h_relu1 = self.slice1(X)
+        return h_relu1
+
+@LOSS_REGISTRY.register()
+class Stripformer_Loss(nn.Module):
+
+    def __init__(self, loss_weight=1.0, reduction='mean', eps=1e-12):
+        super(Stripformer_Loss, self).__init__()
+
+        self.char = CharbonnierLoss()
+        self.edge = EdgeLoss()
+        self.contrastive = ContrastLoss()
+
+    def forward(self, restore, sharp, blur):
+        char = self.char(restore, sharp)
+        edge = 0.05 * self.edge(restore, sharp)
+        contrastive = 0.0005 * self.contrastive(restore, sharp, blur)
+        loss = char + edge + contrastive
+        return loss
+
+@LOSS_REGISTRY.register()
+class PSNRLoss(nn.Module):
+
+    def __init__(self, loss_weight=1.0, reduction='mean', toY=False):
+        super(PSNRLoss, self).__init__()
+        assert reduction == 'mean'
+        self.loss_weight = loss_weight
+        self.scale = 10 / np.log(10)
+        self.toY = toY
+        self.coef = torch.tensor([65.481, 128.553, 24.966]).reshape(1, 3, 1, 1)
+        self.first = True
+
+    def forward(self, pred, target, mask=None, weight=None):
+        loss = 0
+        assert len(pred.size()) == 4
+        if mask is not None and weight is not None:
+            loss += self.loss_weight * self.scale * torch.log(((pred*mask - target*mask) ** 2).mean(dim=(1, 2, 3)) + 1e-8).mean() * weight
+            loss += self.loss_weight * self.scale * torch.log(((pred*(1 - mask) - target*(1 - mask)) ** 2).mean(dim=(1, 2, 3)) + 1e-8).mean() * (1-weight)
+        else:
+            loss += self.loss_weight * self.scale * torch.log(((pred - target) ** 2).mean(dim=(1, 2, 3)) + 1e-8).mean()
+        return loss
+
+
+@LOSS_REGISTRY.register()
+class MSEWeightLoss(nn.Module):
+
+    def __init__(self, loss_weight=1.0, reduction='mean', toY=False):
+        super(MSEWeightLoss, self).__init__()
+        assert reduction == 'mean'
+        self.loss_weight = loss_weight
+        self.first = True
+
+    def forward(self, pred, target, mask=None, weight=None):
+        loss = 0
+        assert len(pred.size()) == 4
+        if mask is not None and weight is not None:
+            loss += self.loss_weight * ((pred*mask - target*mask) ** 2).mean() * weight
+            loss += self.loss_weight * ((pred*(1 - mask) - target*(1 - mask)) ** 2).mean() * (1-weight)
+        else:
+            loss += self.loss_weight * ((pred - target) ** 2).mean()
+        return loss
+
+def gaussian(window_size, sigma):
+    gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
+    return gauss / gauss.sum()
+
+
+def create_window(window_size, channel):
+    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+    window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
+    return window
+
+
+def _ssim(img1, img2, window, window_size, channel, size_average=False, weight=None):
+    mu1 = F.conv2d(img1, window, padding=window_size // 2, groups=channel)
+    mu2 = F.conv2d(img2, window, padding=window_size // 2, groups=channel)
+
+    mu1_sq = mu1.pow(2)
+    mu2_sq = mu2.pow(2)
+    mu1_mu2 = mu1 * mu2
+
+    sigma1_sq = F.conv2d(img1 * img1, window, padding=window_size // 2, groups=channel) - mu1_sq
+    sigma2_sq = F.conv2d(img2 * img2, window, padding=window_size // 2, groups=channel) - mu2_sq
+    sigma12 = F.conv2d(img1 * img2, window, padding=window_size // 2, groups=channel) - mu1_mu2
+
+    C1 = 0.01 ** 2
+    C2 = 0.03 ** 2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2))
+
+    if weight is not None:
+        # weight = weight[:, :, 5:-5, 5:-5]
+        return (ssim_map * weight).sum([1, 2, 3]) / weight.sum([1, 2, 3]).clamp_min(1) / img1.shape[1]
+
+    if size_average:
+        return (-1) * ssim_map.mean()
+    else:
+        return (-1) * ssim_map.mean(1).mean(1).mean(1)
+
+
+@LOSS_REGISTRY.register()
+class SSIMLoss(torch.nn.Module):
+    def __init__(self, window_size=11, size_average=True,  reduction='mean'):
+        super(SSIMLoss, self).__init__()
+        self.window_size = window_size
+        self.size_average = size_average
+        self.channel = 1
+        self.window = create_window(window_size, self.channel)
+
+    def forward(self, img1, img2):
+        (_, channel, _, _) = img1.size()
+
+        if channel == self.channel and self.window.data.type() == img1.data.type():
+            window = self.window
+        else:
+            window = create_window(self.window_size, channel)
+
+            if img1.is_cuda:
+                window = window.cuda(img1.get_device())
+            window = window.type_as(img1)
+
+            self.window = window
+            self.channel = channel
+
+        return 1 + _ssim(img1, img2, window, self.window_size, channel, self.size_average)
+
+@LOSS_REGISTRY.register()
+class SSIMWeightLoss(torch.nn.Module):
+    def __init__(self, loss_weight=1.0, window_size=11, size_average=False, reduction='mean'):
+        super(SSIMWeightLoss, self).__init__()
+        self.window_size = window_size
+        self.size_average = size_average
+        self.channel = 1
+        self.loss_weight = loss_weight
+        self.window = create_window(window_size, self.channel)
+
+    def forward(self, img1, img2, mask=None):
+        (_, channel, _, _) = img1.size()
+
+        if channel == self.channel and self.window.data.type() == img1.data.type():
+            window = self.window
+        else:
+            window = create_window(self.window_size, channel)
+
+            if img1.is_cuda:
+                window = window.cuda(img1.get_device())
+            window = window.type_as(img1)
+
+            self.window = window
+            self.channel = channel
+
+        return (1 - _ssim(img1, img2, window, self.window_size, channel, self.size_average, mask)) * self.loss_weight
+
+@LOSS_REGISTRY.register()
+class BCELoss(torch.nn.Module):
+    def __init__(self, loss_weight=1.0,  reduction='mean'):
+        super(BCELoss, self).__init__()
+        self.loss_weight = loss_weight
+        self.bce = nn.BCEWithLogitsLoss().cuda()
+
+    def forward(self, img1, img2, mask=None):
+        return self.bce(img1, img2) * self.loss_weight
